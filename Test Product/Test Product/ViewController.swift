0fc65f0c11f034f85a055d19d2e4d6c180baff34
AL Mustakim <>
2021-01-24 21:52:18 +0600

    added an outlet for label, design the label

--- Test Product/Test Product/ViewController.swift
+++ Test Product/Test Product/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        welcomeLabel.text = "Welcome"
+        welcomeLabel.textColor = .black
+        welcomeLabel.backgroundColor = .lightGray
+        
     }
 
 
