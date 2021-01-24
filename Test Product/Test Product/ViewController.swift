3a00254c6437809cb9e1e8211c568a46633efa4a
Al Mustakim <badhonmdabdullah@gmail.com>
2021-01-24 21:10:05 +0600

    it is a basic setup screen
    here it is a basic setup screen with a navigation and image view .

---
+++ Test Product/Test Product/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Product
+//
+//  Created by AL Mustakim on 22/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
